7ff412d917bfc80d86c54760d1cef1aeb4ddd00d
Sumeyra Ozgur <smyrozgur@gmail.com>
2019-06-30 01:12:23 -0700

    Add faker and spec gems  - add fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+	battery_level = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.45, 0.55, 0.67, 1].sample
+
+	Scooter.create!(battery_level: battery_level, lonlat:"POINT(#{Faker::Address.latitude} #{Faker::Address.longitude})")
+end
